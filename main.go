bd412c05da917a3940e66f979bd1c5ceb65df89c
taesun_lee <taesun_lee@tmax.co.kr>
2020-11-04 15:35:49 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+var httpRequestCount int = 0
+
+func main() {
+	http.HandleFunc("/test", myHandler)
+
+	http.ListenAndServe(":5000", nil)
+}
+
+func myHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Printf("Current httpRequestCount : %d\n", httpRequestCount)
+
+	httpRequestCount++
+
+	var result string = "Hello Tmax, Request Count : " + strconv.Itoa(httpRequestCount)
+
+	w.Write([]byte(result))
+}
